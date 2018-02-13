6260757b0f0430e23527cac60ace003e9e82f031
esselesse <wikkky@mail.ru>
2018-02-13 04:15:55 +0300

    gitignored

--- firstTodo/src/js/constants/FilterTypes.js
+++
@@ -1,7 +0,0 @@
-var CONSTS = {
-    "FILTER_ALL": 'todos-filter __all',
-    "FILTER_COMPLETED": 'todos-filter __completed',
-    "FILTER_ACTIVE": 'todos-filter __active'
-};
-
-module.exports = CONSTS;
